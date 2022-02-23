40e1f2dd0cc10b472fc58c0b34d2fe8bdcf8ab60
qujiahao <qujiahao@qq.com>
2022-02-23 10:33:29 +0800

    perf: modify demo

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import naive from 'naive-ui';
 import App from './App.vue';
 import router from './router';
 import { setupStore } from '@/store';
-// import { useUserStoreWidthOut } from '@/store/modules/user';
-// import { useDesignStoreWidthOut } from '@/store/modules/design';
-// import { isBoolean } from 'natural-utils/lib/is';
 
 const app = createApp(App);
 app.use(naive);
@@ -16,8 +13,6 @@ app.use(router);
 app.mount('#app');
 
 function dataListener(data: any) {
-  console.log('vue3-app来自基座应用的数据', data);
-
   /**
    * router
    */
