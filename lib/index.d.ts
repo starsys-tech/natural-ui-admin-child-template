a4700c5beb2d5fd2e3aca514c5534b615f659eee
qujiahao <qujiahao@qq.com>
2022-02-28 15:01:50 +0800

    fix: component types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,5 @@
 import { DefineComponent } from 'vue';
 
-export declare type SpriteIcon = DefineComponent<{
-  readonly width?: number;
-  readonly height?: number;
-  readonly image: string;
-  readonly backgroundWidth?: number;
-  readonly duration?: number;
-  readonly steps?: number;
+export declare type HelloWorld = DefineComponent<{
+  readonly content?: string;
 }>;
